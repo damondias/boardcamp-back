d32c3cae77db145e5725fdca8cf51453f3272dba
Damon <damondias@gmail.com>
2023-07-29 11:04:28 -0400

    feat: routes architecture

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express, { json } from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import router from "./routes/index.routes.js";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(json());
+app.use(router);
 
 const PORT = process.env.PORT;
 
